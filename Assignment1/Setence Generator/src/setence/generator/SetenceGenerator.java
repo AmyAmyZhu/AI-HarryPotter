ecc356ac9294948ee592760ce631992844f0152f
Justin Hu <j62hu@uwaterloo.ca>
2016-10-07 11:43:18 -0400

    updated part4

--- Assignment1/Setence Generator/src/setence/generator/SetenceGenerator.java
+++ Assignment1/Setence Generator/src/setence/generator/SetenceGenerator.java
@@ -45,8 +45,47 @@ public class SetenceGenerator {
 			
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		
+		}	
     }
-    
 }
+
+/*	BFS
+ * "benjamin saw the apple"with probability 0.001211240310
+Total nodes considered: 996
+
+"a son thanked god"with probability 0.000132052425
+Total nodes considered: 358
+
+"benjamin saw the youngest son"with probability 0.000127053179379
+Total nodes considered: 82
+
+"a son thanked god for the apple"with probability 0.000000070885699762341
+Total nodes considered: 19246
+
+*	DFS
+"benjamin saw the water"with probability 0.001211240310
+Total nodes considered: 996
+
+"a son thanked god"with probability 0.000132052425
+Total nodes considered: 358
+
+"benjamin saw the youngest son"with probability 0.000127053179379
+Total nodes considered: 65
+
+"a son thanked god for the water"with probability 0.000000070885699762341
+Total nodes considered: 12454
+
+*	HEURISTIC
+"benjamin saw the child"with probability 0.000512447823
+Total nodes considered: 996
+
+"a man said jack"with probability 0.000082286682
+Total nodes considered: 234
+
+"benjamin saw the youngest son"with probability 0.000127053179379
+Total nodes considered: 64
+
+"a son thanked god for a man"with probability 0.000000046145882557744
+Total nodes considered: 17150
+
+*/
